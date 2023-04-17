d0dd4fc400f9e0048af7b7aa5c7b6159613ccdae
Evgen Dirt <bankir032@mail.ru>
2023-04-17 20:40:20 +0300

    first commit

---
+++ 11.py
@@ -0,0 +1,17 @@
+
+n = int(input("Введите число: "))
+i = 1
+fact = 1
+while i <= n:
+    fact *= i
+    i += 1
+print(fact)
+
+# n = int (input ("Введите число "))
+# i = 1
+# fact = 1
+
+# while i <= n:
+#     fact *= i
+#     i += 1
+# print (fact)    
